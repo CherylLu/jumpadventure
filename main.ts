22457c6c3b3d6a1b9fc15d223f36c880f0c9fbd6
CherylLu <35189430+CherylLu@users.noreply.github.com>
2024-07-29 16:07:37 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,348 @@
+namespace SpriteKind {
+    export const trampoline = SpriteKind.create()
+}
+// 設定主角的回彈速度
+sprites.onOverlap(SpriteKind.Player, SpriteKind.trampoline, function (sprite, otherSprite) {
+    mySprite.vy = -100
+    otherSprite.destroy()
+    彈跳墊 = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . f f f f f f f f f f f f f f . 
+        . . . . . f . . . . f f . . . . 
+        . . . . f . . . . . . f . . . . 
+        . . . . f . . . . . . . f . . . 
+        . . . f . . . . . . . . . f . . 
+        . . f f . . . . . . . . . f f . 
+        . . f f . . . . . . . . . f f . 
+        . . . f f f . . . . . . f f . . 
+        . . . . . f f . . . f f f . . . 
+        . . . . . . f f . f f . . . . . 
+        . f f f f f f f f f f f f f f . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.trampoline)
+})
+// 設定向左的動畫效果
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    mySprite,
+    [img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . b 5 5 b . . . . . . . . . 
+        . . . . b b b b b b . . . . . . 
+        . . . b 5 5 5 5 5 b b . . . . . 
+        . . b 5 5 5 5 5 5 5 b b b b b . 
+        . . b 5 5 5 5 5 5 5 5 b 5 d b . 
+        . . f 4 d 5 f 1 d 5 b 5 5 b . . 
+        . . c 4 4 5 f f 1 b 5 5 d b . . 
+        . b 4 4 4 4 b f d 5 5 5 b d b b 
+        b 4 4 4 4 4 4 5 b 5 5 d c d d b 
+        . b 5 5 5 5 5 5 5 b c c d d d c 
+        . b 5 5 5 5 5 5 5 d d d d d b c 
+        . b d 5 5 5 5 5 d d d d d d c . 
+        . . b b 5 5 5 d d d d d b c . . 
+        . . . b b c c c c c c c c . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . b 5 b . . . . . . . . . . 
+        . . . . b 5 b . . . . . . . . . 
+        . . . . b b b b b b . . . . . . 
+        . . . b 5 5 5 5 5 b b . . . . . 
+        . . b 5 5 5 5 5 5 5 b b b b b . 
+        . . b 5 5 5 5 5 5 5 5 b 5 d b . 
+        . . f 4 d 5 f 1 d 5 b 5 5 b . . 
+        . . c 4 4 5 f f 1 b 5 5 d b . . 
+        b 4 4 4 4 4 b f d 5 5 5 b d b b 
+        . b 4 4 4 4 4 5 b 5 5 d c d d b 
+        . b 5 5 5 5 5 5 5 b c c d d d c 
+        . b 5 5 5 5 5 5 5 d d d d d b c 
+        . b d 5 5 5 5 5 d d d d d d c . 
+        . . b b 5 5 5 d d d d d b c . . 
+        . . . b b c c c c c c c c . . . 
+        `,img`
+        . . . b 5 b . . . . . . . . . . 
+        . . . . b 5 b . . . . . . . . . 
+        . . . . . c b . . . . . . . . . 
+        . . . . b b b b b b . . . . . . 
+        . . . b 5 5 5 5 5 b b . . . . . 
+        . . f d 5 5 f 1 d 5 b b . . . . 
+        . . c 4 d 5 f f 1 5 5 b . . . . 
+        . . 4 4 d d b f d 5 5 b . . . . 
+        b 4 4 4 4 4 5 5 5 d b b d d d b 
+        . b 4 4 4 4 4 5 5 b 5 5 5 d b b 
+        . . b 5 5 5 5 5 d 5 5 5 5 c d b 
+        . b 5 5 5 5 5 5 b 5 5 d c d d c 
+        . b 5 5 5 5 5 5 5 b c c d d b c 
+        . b d 5 5 5 5 5 d d d d d d c . 
+        . . b b 5 5 5 d d d d d b c . . 
+        . . . b b c c c c c c c c . . . 
+        `,img`
+        . . . b 5 b . . . . . . . . . . 
+        . . . . b 5 b . . . . . . . . . 
+        . . . . b b b b b b . . . . . . 
+        . . . b 5 5 5 5 5 b b . . . . . 
+        . . c 4 d 5 f 1 d 5 b b . . . . 
+        b 4 4 4 d d f f 1 5 5 b . . . . 
+        . b 4 4 4 4 b f d 5 5 b . . . . 
+        . . b 4 4 4 4 5 5 5 5 d b . . . 
+        . . b 5 5 5 5 5 5 5 5 d d b . . 
+        . b 5 5 5 5 5 5 5 5 d d d d b . 
+        . b 5 5 5 5 5 5 5 b b b d d d b 
+        . b 5 5 5 5 5 5 c d 5 5 b d d c 
+        . b 5 5 5 5 5 5 d c d 5 d b b c 
+        . b d 5 5 5 5 5 d d c b 5 5 b . 
+        . . b b 5 5 5 d d d d c c c b b 
+        . . . b b c c c c c c c c . . . 
+        `,img`
+        . . . b 5 b . . . . . . . . . . 
+        . . . . b 5 b . . . . . . . . . 
+        . . . . b b b b b b . . . . . . 
+        . . . b 5 5 5 5 5 b b . . . . . 
+        . . c 4 d 5 f 1 d 5 b b . . . . 
+        b 4 4 4 d d f f 1 5 5 b . . . . 
+        . b 4 4 4 4 b f d 5 5 b . . . . 
+        . . b 4 4 4 4 5 5 5 5 d b . . . 
+        . . b 5 5 5 5 5 5 5 d d d b b . 
+        . b 5 5 5 5 5 5 5 b b b d d d b 
+        . b 5 5 5 5 5 5 c d 5 5 b d d c 
+        . b 5 5 5 5 5 5 d c d 5 d b b c 
+        . b 5 5 5 5 5 5 d d c b 5 5 b c 
+        . b d 5 5 5 5 5 d d d c c c b b 
+        . . b b 5 5 5 d d d c c . . . . 
+        . . . b b c c c c c . . . . . . 
+        `,img`
+        . . . b 5 b . . . . . . . . . . 
+        . . . . b 5 b . . . . . . . . . 
+        . . . . b b b b b b . . . . . . 
+        . . . b 5 5 5 5 5 b b . . . . . 
+        . . f d 5 5 f 1 d 5 b b . . . . 
+        . . c 4 d 5 f f 1 5 5 b . . . . 
+        . . 4 4 d d b f d 5 5 b . . . . 
+        b 4 4 4 4 4 5 5 5 5 5 d b b b . 
+        . b 4 4 4 4 4 5 5 d b b d d d b 
+        . . b 5 5 5 5 5 5 b 5 5 5 d b b 
+        . b 5 5 5 5 5 5 d 5 5 5 5 c d c 
+        . b 5 5 5 5 5 5 b 5 5 d c d b c 
+        . b d 5 5 5 5 5 d b c c d d c . 
+        . . b b 5 5 5 d d d d d b c . . 
+        . . . b b c c c c c c c c . . . 
+        . . . . . . . . . . . . . . . . 
+        `],
+    100,
+    true
+    )
+})
+// 設定向右動畫效果
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    mySprite,
+    [img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . b 5 5 b . . . 
+        . . . . . . b b b b b b . . . . 
+        . . . . . b b 5 5 5 5 5 b . . . 
+        . b b b b b 5 5 5 5 5 5 5 b . . 
+        . b d 5 b 5 5 5 5 5 5 5 5 b . . 
+        . . b 5 5 b 5 d 1 f 5 d 4 f . . 
+        . . b d 5 5 b 1 f f 5 4 4 c . . 
+        b b d b 5 5 5 d f b 4 4 4 4 b . 
+        b d d c d 5 5 b 5 4 4 4 4 4 4 b 
+        c d d d c c b 5 5 5 5 5 5 5 b . 
+        c b d d d d d 5 5 5 5 5 5 5 b . 
+        . c d d d d d d 5 5 5 5 5 d b . 
+        . . c b d d d d d 5 5 5 b b . . 
+        . . . c c c c c c c c b b . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . b 5 b . . . 
+        . . . . . . . . . b 5 b . . . . 
+        . . . . . . b b b b b b . . . . 
+        . . . . . b b 5 5 5 5 5 b . . . 
+        . b b b b b 5 5 5 5 5 5 5 b . . 
+        . b d 5 b 5 5 5 5 5 5 5 5 b . . 
+        . . b 5 5 b 5 d 1 f 5 d 4 f . . 
+        . . b d 5 5 b 1 f f 5 4 4 c . . 
+        b b d b 5 5 5 d f b 4 4 4 4 4 b 
+        b d d c d 5 5 b 5 4 4 4 4 4 b . 
+        c d d d c c b 5 5 5 5 5 5 5 b . 
+        c b d d d d d 5 5 5 5 5 5 5 b . 
+        . c d d d d d d 5 5 5 5 5 d b . 
+        . . c b d d d d d 5 5 5 b b . . 
+        . . . c c c c c c c c b b . . . 
+        `,img`
+        . . . . . . . . . . b 5 b . . . 
+        . . . . . . . . . b 5 b . . . . 
+        . . . . . . . . . b c . . . . . 
+        . . . . . . b b b b b b . . . . 
+        . . . . . b b 5 5 5 5 5 b . . . 
+        . . . . b b 5 d 1 f 5 5 d f . . 
+        . . . . b 5 5 1 f f 5 d 4 c . . 
+        . . . . b 5 5 d f b d d 4 4 . . 
+        b d d d b b d 5 5 5 4 4 4 4 4 b 
+        b b d 5 5 5 b 5 5 4 4 4 4 4 b . 
+        b d c 5 5 5 5 d 5 5 5 5 5 b . . 
+        c d d c d 5 5 b 5 5 5 5 5 5 b . 
+        c b d d c c b 5 5 5 5 5 5 5 b . 
+        . c d d d d d d 5 5 5 5 5 d b . 
+        . . c b d d d d d 5 5 5 b b . . 
+        . . . c c c c c c c c b b . . . 
+        `,img`
+        . . . . . . . . . . b 5 b . . . 
+        . . . . . . . . . b 5 b . . . . 
+        . . . . . . b b b b b b . . . . 
+        . . . . . b b 5 5 5 5 5 b . . . 
+        . . . . b b 5 d 1 f 5 d 4 c . . 
+        . . . . b 5 5 1 f f d d 4 4 4 b 
+        . . . . b 5 5 d f b 4 4 4 4 b . 
+        . . . b d 5 5 5 5 4 4 4 4 b . . 
+        . . b d d 5 5 5 5 5 5 5 5 b . . 
+        . b d d d d 5 5 5 5 5 5 5 5 b . 
+        b d d d b b b 5 5 5 5 5 5 5 b . 
+        c d d b 5 5 d c 5 5 5 5 5 5 b . 
+        c b b d 5 d c d 5 5 5 5 5 5 b . 
+        . b 5 5 b c d d 5 5 5 5 5 d b . 
+        b b c c c d d d d 5 5 5 b b . . 
+        . . . c c c c c c c c b b . . . 
+        `,img`
+        . . . . . . . . . . b 5 b . . . 
+        . . . . . . . . . b 5 b . . . . 
+        . . . . . . b b b b b b . . . . 
+        . . . . . b b 5 5 5 5 5 b . . . 
+        . . . . b b 5 d 1 f 5 d 4 c . . 
+        . . . . b 5 5 1 f f d d 4 4 4 b 
+        . . . . b 5 5 d f b 4 4 4 4 b . 
+        . . . b d 5 5 5 5 4 4 4 4 b . . 
+        . b b d d d 5 5 5 5 5 5 5 b . . 
+        b d d d b b b 5 5 5 5 5 5 5 b . 
+        c d d b 5 5 d c 5 5 5 5 5 5 b . 
+        c b b d 5 d c d 5 5 5 5 5 5 b . 
+        c b 5 5 b c d d 5 5 5 5 5 5 b . 
+        b b c c c d d d 5 5 5 5 5 d b . 
+        . . . . c c d d d 5 5 5 b b . . 
+        . . . . . . c c c c c b b . . . 
+        `,img`
+        . . . . . . . . . . b 5 b . . . 
+        . . . . . . . . . b 5 b . . . . 
+        . . . . . . b b b b b b . . . . 
+        . . . . . b b 5 5 5 5 5 b . . . 
+        . . . . b b 5 d 1 f 5 5 d f . . 
+        . . . . b 5 5 1 f f 5 d 4 c . . 
+        . . . . b 5 5 d f b d d 4 4 . . 
+        . b b b d 5 5 5 5 5 4 4 4 4 4 b 
+        b d d d b b d 5 5 4 4 4 4 4 b . 
+        b b d 5 5 5 b 5 5 5 5 5 5 b . . 
+        c d c 5 5 5 5 d 5 5 5 5 5 5 b . 
+        c b d c d 5 5 b 5 5 5 5 5 5 b . 
+        . c d d c c b d 5 5 5 5 5 d b . 
+        . . c b d d d d d 5 5 5 b b . . 
+        . . . c c c c c c c c b b . . . 
+        . . . . . . . . . . . . . . . . 
+        `],
+    100,
+    true
+    )
+})
+sprites.onCreated(SpriteKind.trampoline, function (sprite) {
+    sprite.x = randint(0, scene.screenWidth())
+    sprite.y = randint(30, scene.screenHeight())
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    otherSprite.destroy()
+    game.gameOver(false)
+})
+// 設定主角的加速度
+let shark: Sprite = null
+let 彈跳墊: Sprite = null
+let mySprite: Sprite = null
+scene.setBackgroundColor(9)
+mySprite = sprites.create(img`
+    . . . . . . . . . b 5 b . . . . 
+    . . . . . . . . . b 5 b . . . . 
+    . . . . . . b b b b b b . . . . 
+    . . . . . b b 5 5 5 5 5 b . . . 
+    . . . . b b 5 b c 5 5 d 4 c . . 
+    . b b b b 5 5 5 b f d d 4 4 4 b 
+    . b d 5 b 5 5 b c b 4 4 4 4 b . 
+    . . b 5 5 b 5 5 5 4 4 4 4 b . . 
+    . . b d 5 5 b 5 5 5 5 5 5 b . . 
+    . b d b 5 5 5 d 5 5 5 5 5 5 b . 
+    b d d c d 5 5 b 5 5 5 5 5 5 b . 
+    c d d d c c b 5 5 5 5 5 5 5 b . 
+    c b d d d d d 5 5 5 5 5 5 5 b . 
+    . c d d d d d d 5 5 5 5 5 d b . 
+    . . c b d d d d d 5 5 5 b b . . 
+    . . . c c c c c c c c b b . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(mySprite, 100, 0)
+mySprite.y = 0
+mySprite.ay = 100
+for (let index = 0; index < 3; index++) {
+    彈跳墊 = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . f f f f f f f f f f f f f f . 
+        . . . . . f . . . . f f . . . . 
+        . . . . f . . . . . . f . . . . 
+        . . . . f . . . . . . . f . . . 
+        . . . f . . . . . . . . . f . . 
+        . . f f . . . . . . . . . f f . 
+        . . f f . . . . . . . . . f f . 
+        . . . f f f . . . . . . f f . . 
+        . . . . . f f . . . f f f . . . 
+        . . . . . . f f . f f . . . . . 
+        . f f f f f f f f f f f f f f . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.trampoline)
+}
+game.onUpdate(function () {
+    if (mySprite.y > 150) {
+        game.over(false)
+    }
+})
+game.onUpdateInterval(5000, function () {
+    shark = sprites.create(img`
+        ................
+        ................
+        ................
+        ................
+        ................
+        ................
+        ................
+        ................
+        ................
+        ................
+        ..........fffff.
+        ...ccc...ff1bbbf
+        ..c1cccccccc1bbf
+        ..c113333c1c11bf
+        .c11333333cc11bf
+        .c11c133331c11bf
+        .c111cccc3c11bbf
+        .f111111111ffbbf
+        .ffcc11bbbbffbf.
+        .fbbbbbbbbbbbbf.
+        fbdbbbbbbbcbbbc.
+        fdbbbbbbccbbbfbc
+        fbbfcbbbbbcbbfbc
+        fbfdccbbccbbfbbf
+        ffddcccbbbccffbf
+        .cddcccccccf..ff
+        .cddcccccccf....
+        ..cdbcccccf.....
+        ...cbccccf......
+        ....cfccf.......
+        ...ffbbdc.......
+        ..fbccbddc......
+        .fbbcccbdc......
+        fbbcffccbc......
+        fbff...cbc......
+        ff......cc......
+        `, SpriteKind.Enemy)
+    shark.setPosition(randint(0, scene.screenWidth()), randint(30, scene.screenHeight()))
+})
